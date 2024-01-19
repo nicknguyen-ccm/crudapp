05303f69d42521b15d5595bf8183939a59abf45c
nicknguyen-ccm <Nick.Nguyen@ccm.com>
2024-01-19 13:41:11 -0500

    Update HelloWorld.vue
    fixed sorting logic

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,15 +6,17 @@ type Post = {
   isComplete: boolean,
   todoName: string
 }
+type SortType = 'ascend' | 'descend' | 'none'
 let posts = ref<Post[]>([])
 const title = ref('')
 const isComplete = ref(false)
 const filterQuery = ref('')
+const sortMode = ref<SortType>('none')
 
 
 onMounted( () => {
   retrievePosts()
-}) 
+})
 function retrievePosts() {
   axios.get('https://calm-plum-jaguar-tutu.cyclic.app/todos').then((result) => {
     posts.value = result.data.data
@@ -22,8 +24,8 @@ function retrievePosts() {
   }).catch((error) => {console.log('error',error)})
 }
 function handleSubmit() {
-  
-  if (title.value === ''){ 
+
+  if (title.value === ''){
     alert('invalid title')
   } else {
     axios.post('https://calm-plum-jaguar-tutu.cyclic.app/todos',
@@ -65,87 +67,95 @@ function deleteHandler(id:string) {
   })
 }
 
-function sort(mode:string) {
-  if (mode === 'ascend') {
-    posts.value = posts.value.sort((a,b) => {
-      if (a.todoName < b.todoName) {
+function sort(mode:SortType) {
+  sortMode.value = mode
+}
+
+
+const filteredPosts = computed(() => {
+  if (sortMode.value === 'none') {
+    return posts.value.filter((post) => {
+      return post.todoName.includes(filterQuery.value.toLowerCase())
+  })} else if (sortMode.value === 'ascend') {
+    return posts.value.filter((post) => {
+      return post.todoName.includes(filterQuery.value.toLowerCase())
+  }).sort((a,b) => {
+      if (a.todoName.toLowerCase()  < b.todoName.toLowerCase()) {
         return -1;
       }
-      if (a.todoName > b.todoName) {
+      if (a.todoName.toLowerCase() > b.todoName.toLowerCase()) {
         return 1;
       }
       return 0;
     })
   } else {
-    posts.value = posts.value.sort((a,b) => {
-      if (a.todoName < b.todoName) {
+    return posts.value.filter((post) => {
+      return post.todoName.includes(filterQuery.value.toLowerCase())
+    }).sort((a,b) => {
+      if (a.todoName.toLowerCase() < b.todoName.toLowerCase() ) {
         return 1;
       }
-      if (a.todoName > b.todoName) {
+      if (a.todoName.toLowerCase()  > b.todoName.toLowerCase() ) {
         return -1;
       }
       return 0;
     })
   }
-}
- 
-const filteredPosts = computed(() => {
-  return posts.value.filter((post) => {
-    return post.todoName.includes(filterQuery.value)
-  })
-})
+  }
+
+)
 </script>
 
 <template>
-<div class="formAndTable">
-  <div>
-  <label>Filter: </label>
-  <input type="text" v-model="filterQuery">
-  <table>
-    <th>Title
-      <button @click="sort('descend')"><font-awesome-icon :icon="['fas', 'arrow-up']"/></button>
-      <button @click="sort('ascend')"><font-awesome-icon :icon="['fas', 'arrow-down']"/></button>
-
-    </th>
-    <th>Complete
-
-      
-    </th>
-    <th>Actions</th>
-    <tr v-for="post in filteredPosts" :key="post._id">
-      <td>{{ post.todoName }} </td>
-      <td>
-        <input type="checkbox" v-bind:checked="post.isComplete" v-on:change="postCheck($event, post._id, post.isComplete)">
-      </td>
-      <td>
-        <button v-on:click="deleteHandler(post._id)">Delete</button>
-      </td>
-    </tr>
-  </table>
-</div>
-  <div class="submitForm">
-    <label>Title: </label>
-    <input type="text" v-model.trim="title">
-    <label>Complete: </label>
-    <input type="checkbox" v-model="isComplete">
-    <button @click="handleSubmit">Submit</button>
+  <div class="formAndTable">
+    <div>
+      <label>Filter: </label>
+      <input type="text" v-model="filterQuery" />
+      <table>
+        <th>
+          Title
+          <button @click="sort('descend')">
+            <font-awesome-icon :icon="['fas', 'arrow-up']" />
+          </button>
+          <button @click="sort('ascend')">
+            <font-awesome-icon :icon="['fas', 'arrow-down']" />
+          </button>
+        </th>
+        <th>Complete</th>
+        <th>Actions</th>
+        <tr v-for="post in filteredPosts" :key="post._id">
+          <td>{{ post.todoName }}</td>
+          <td>
+            <input
+              type="checkbox"
+              v-bind:checked="post.isComplete"
+              v-on:change="postCheck($event, post._id, post.isComplete)"
+            />
+          </td>
+          <td>
+            <button v-on:click="deleteHandler(post._id)">Delete</button>
+          </td>
+        </tr>
+      </table>
+    </div>
+    <div class="submitForm">
+      <label>Title: </label>
+      <input type="text" v-model.trim="title" />
+      <label>Complete: </label>
+      <input type="checkbox" v-model="isComplete" />
+      <button @click="handleSubmit">Submit</button>
+    </div>
   </div>
-</div>
-
-
-
-
 </template>
 
 <style scoped>
-.submitForm { 
+.submitForm {
   display: flex;
   flex-direction: column;
-  flex-basis:auto;
+  flex-basis: auto;
   background-color: rgb(164, 172, 182);
   padding: 10px;
   border-radius: 15px;
-  
 }
 table {
   background-color: rgb(182, 201, 231);
@@ -153,14 +163,13 @@ table {
   padding: 10px;
 }
 .th {
-  background-color: rgb(202, 164, 164)
+  background-color: rgb(202, 164, 164);
 }
 .formAndTable {
   display: flex;
-  background-color:rgb(184, 189, 197);
+  background-color: rgb(184, 189, 197);
   border-radius: 15px;
   padding: 10px;
-  gap: 20px
+  gap: 20px;
 }
-
 </style>
